788d17ac2aed90b35dc4869dc0352bd840a8dce6
Luu Phuc <luuphuc6297@gmail.com>
2023-09-11 14:03:14 +0700

    setup ci/cd

--- cypress/e2e/SeoMetadata.cy.ts
+++ cypress/e2e/SeoMetadata.cy.ts
@@ -1,7 +1,7 @@
 describe('Seo metadata', () => {
     describe('Verify SEO Metadata', () => {
         it('should render SEO metadata on Index page', () => {
-            cy.visit('/');
+            cy.visit('/', { timeout: 10000 });
 
             // The Index page should have a page title
             cy.title().should('not.be.empty');
